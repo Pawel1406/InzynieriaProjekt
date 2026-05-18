2e5b63bae02f3ed38189c3c547c5df2874270c05
Pawel1406 <platyn14@gmail.com>
2026-05-18 19:01:16 +0200

    proba

--- src/Bron.java
+++ src/Bron.java
@@ -1,2 +1,21 @@
 public abstract class Bron {
+    private String nazwa;
+    private boolean sprawna;
+
+    public Bron(String nazwa) {
+        this.nazwa = nazwa;
+        this.sprawna = true;
+    }
+
+    public boolean isSprawna() {
+        return sprawna;
+    }
+
+    public void setSprawna(boolean sprawna) {
+        this.sprawna = sprawna;
+    }
+
+    public abstract void uzyj();
+
+    public abstract void uzyj(int iloscStrzalow);
 }
